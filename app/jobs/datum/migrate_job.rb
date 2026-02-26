43b06b49ab200702d679195f198c37f264759e64
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-26 15:42:12 +0800

    fix migrate job

--- app/jobs/datum/migrate_job.rb
+++ app/jobs/datum/migrate_job.rb
@@ -2,9 +2,9 @@ module Datum
   class MigrateJob < ApplicationJob
     queue_as :default
 
-    def perform(record_klass, target)
+    def perform(record_klass, target_name)
       record_class = record_klass.constantize
-      record_class.migrate(target)
+      record_class.migrate(record_class.const_get(target_name))
     end
 
   end
