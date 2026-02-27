982e09ad256fe2d368aa12a7d9a6d1aaf1ac5030
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-27 10:03:58 +0800

    try with continue

--- app/jobs/datum/migrate_job.rb
+++ app/jobs/datum/migrate_job.rb
@@ -4,7 +4,13 @@ module Datum
 
     def perform(record_klass, target_name)
       record_class = record_klass.constantize
-      record_class.migrate(record_class.const_get(target_name))
+
+      step :process, start: record_class.first.id do |step|
+        find_each(start: step.cursor) do |i|
+          i.migrate(record_class.const_get(target_name))
+          step.set! i.id
+        end
+      end
     end
 
   end
