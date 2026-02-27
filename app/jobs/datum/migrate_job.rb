70e0439bdde681fbe8456f558f69eb2027e84257
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-27 14:21:51 +0800

    record class

--- app/jobs/datum/migrate_job.rb
+++ app/jobs/datum/migrate_job.rb
@@ -6,7 +6,7 @@ module Datum
       record_class = record_klass.constantize
 
       step :process, start: record_class.first.id do |step|
-        find_each(start: step.cursor) do |i|
+        record_class.find_each(start: step.cursor) do |i|
           i.migrate(record_class.const_get(target_name))
           step.set! i.id
         end
