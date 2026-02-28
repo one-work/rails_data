400bf921398f68aaff81a5ef4e2d73006cf98426
qinmingyuan <mingyuan0715@foxmail.com>
2026-02-28 15:51:40 +0800

    supprot tagrget

--- app/jobs/datum/migrate_job.rb
+++ app/jobs/datum/migrate_job.rb
@@ -4,11 +4,19 @@ module Datum
 
     def perform(record_klass, target_name)
       record_class = record_klass.constantize
+      target = record_class.const_get(target_name)
 
       step :process, start: record_class.first.id do |step|
-        record_class.find_each(start: step.cursor) do |i|
-          i.migrate(record_class.const_get(target_name))
-          step.set! i.id
+        if target[:not].present?
+          record_class.where(target[:filter] || {}).where.not(target[:not]).find_each(start: step.cursor) do |i|
+            i.migrate(target)
+            step.set! i.id
+          end
+        else
+          record_class.where(target[:filter] || {}).find_each(start: step.cursor) do |i|
+            i.migrate(target)
+            step.set! i.id
+          end
         end
       end
     end
